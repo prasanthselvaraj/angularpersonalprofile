e18c72b81943c0e65680760e6bf3daf6247f621e
prasanthselvaraj <prasanthselvaraj10101994@gmail.com>
2021-05-05 20:50:47 +0530

    Refs Resume page 2nd commit

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -21,4 +21,12 @@ export class ResumeComponent implements OnInit {
   year: String = '2019 - present';
   presentCompany: String = 'Currently working for Ford Motor Company Asia Pacific as Software Engineer from September 2019'+
   ' which provides best environment for learning, innovation and business growth on automobile industry.';
+
+  prevDesig : String = 'Junior Engineer';
+  prevYoe : String = '2016 - 2019';
+  prevComp : String = 'Worked for Ivtl Infoview Technologies Pvt Ltd Chennai as Junior Engineer from July 2016 to August 2019' +
+  ' which provided a base foundation for learning, innovation and business growth on an ERP product.'
+
+  educationPara: String = 'Completed Bachelor of Engineering in Electronics and Communication Engineering' +
+  ' from Sri Krishna College of Engineering and Technology at Coimbatore in the year 2016 with 91.1%.'
 }

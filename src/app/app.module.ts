02b0622fe4b69b59273e23400bb3cc322ae2747e
prasanthselvaraj <prasanthselvaraj10101994@gmail.com>
2021-05-06 21:55:01 +0530

    refs project code change commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,6 +20,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
+    NgImageSliderModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
       {path: 'resume', component: ResumeComponent},
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,4 +12,34 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  projOneTitle: String = "Project Name";
+  projNameOne: String = "OVS (Optimized Vehicle scheduling)";
+  projNameTwo: String = "OSS (Optessa scheduling and Sequencing)";
+  projOneDesc: String = "Project Description";
+  projDescOne: String = 'OVS & OSS is a part of \"Order to delivery\" system which handles the scheduling and'+
+  ' sequencing of unplanned orders based on several constraints, allocations and order life cycle.';
+  projDescTwo: String = 'It’s helpful in tracking the vehicle under manufacturing process until generating the vehicle'+
+  ' identification number and dispatched out of the production plant.';
+
+  imageObject = [{
+      image: '/assets/ford_1.jpg',
+      thumbImage: '/assets/ford_1.jpg',
+      title: 'FORD MOTORS'
+  }, {
+    image: '/assets/ford_1.jpg',
+    thumbImage: '/assets/ford_2.jpg',
+    title: 'FORD MOTORS'
+  }, {
+    image: '/assets/ford_1.jpg',
+    thumbImage: '/assets/ford_3.jpg',
+    title: 'FORD MOTORS'
+  },{
+    image: '/assets/ford_1.jpg',
+    thumbImage: '/assets/ford_4.jpg',
+    title: 'FORD MOTORS'
+  }, {
+    image: '/assets/ford_1.jpg',
+    thumbImage: '/assets/ford_5.jpg',
+    title: 'FORD MOTORS'
+  }];
 }
